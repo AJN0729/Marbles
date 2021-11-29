08ac97bd5b84864d98bbe1ba2a60cde2199f905e
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-11-29 08:49:53 -0600

    added physics.

--- Marbles/GameScene.swift
+++ Marbles/GameScene.swift
@@ -13,10 +13,19 @@ class GameScene: SKScene {
     
     var marbles = ["marbleBlue", "marbleGreen", "marblePurple", "marbleRed", "marbleYellow"]
     
+    let scoreLabel = SKLabelNode(fontNamed: "HelveticaNeue-Thin")
+    
     override func didMove(to view: SKView) {
         
         let marble = SKSpriteNode(imageNamed: "marbleBlue")
         let marbleRadius = marble.frame.width / 2.0
+        
+        scoreLabel.fontSize = 72
+        scoreLabel.position = CGPoint(x: 20, y: 20)
+        scoreLabel.text = "SCORE: 0"
+        scoreLabel.zPosition = 100
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
         
         for i in stride(from: marbleRadius, to: view.bounds.width - marbleRadius, by: marble.frame.width) {
             for j in stride(from: 100, to: view.bounds.height, by: marble.frame.height) {
@@ -24,9 +33,17 @@ class GameScene: SKScene {
                 let marble = Marble(imageNamed: marbleType)
                 marble.position = CGPoint(x: i, y: j)
                 marble.name = marbleType
+                
+                
+                marble.physicsBody = SKPhysicsBody(circleOfRadius: marbleRadius)
+                marble.physicsBody?.allowsRotation = false
+                marble.physicsBody?.friction = 0
+                marble.physicsBody?.restitution = 0
+                
                 addChild(marble)
             }
         }
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame.inset(by: UIEdgeInsets(top: 100, left: 0, bottom: 0, right: 0)))
         
     }
     
